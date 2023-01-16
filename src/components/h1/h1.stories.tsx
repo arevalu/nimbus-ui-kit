e7f51ac6272f9335dcf4a8fa20c878122380e8d8
Luciana Arevalo <arevalolucianadg@gmail.com>
2023-01-16 18:39:11 -0300

    fix: eslint errors

--- src/components/h1/h1.stories.tsx
+++ src/components/h1/h1.stories.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import type { ComponentMeta, ComponentStory } from '@storybook/react-native';
 import { H1 } from './h1';
 
+type H1Type = typeof H1;
+
 export default {
   title: 'Typography/H1',
   component: H1,
-} as ComponentMeta<typeof H1>;
+} as ComponentMeta<H1Type>;
 
-export const Template: ComponentStory<typeof H1> = (args) => (
+export const Template: ComponentStory<H1Type> = args => (
   <H1 {...args}>Some text </H1>
 );
 
 Template.storyName = 'H1';
-
-
--- src/components/h1/h1.tsx
+++ src/components/h1/h1.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components/native'
+import styled from 'styled-components/native';
 
 export const H1 = styled.Text`
   font-family: 'sans-serif';
