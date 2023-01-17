822646f976a930cc406548617fb8fd7e192cb823
Luciana Arevalo <arevalolucianadg@gmail.com>
2023-01-17 20:10:13 -0300

    chore: add color palette

---
+++ src/theme/color-palette.stories.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { ColorPalette, ColorItem } from '@storybook/addon-docs';
+import { Colors } from './colors';
+
+storiesOf('Colors/Palette', module)
+  .addParameters({
+    previewTabs: {
+      canvas: { hidden: true },
+    },
+  })
+  .add('Palette', () => (
+    <ColorPalette>
+      <ColorItem
+        title="Primary | light"
+        subtitle="#000259"
+        colors={{
+          '20': Colors.primary20,
+          '40': Colors.primary40,
+          '60': Colors.primary60,
+          '80': Colors.primary80,
+          '100': Colors.primary100,
+        }}
+      />
+      <ColorItem
+        title="Primary | dark"
+        subtitle="#17183B"
+        colors={{
+          '20': Colors.primaryDark20,
+          '40': Colors.primaryDark40,
+          '60': Colors.primaryDark60,
+          '80': Colors.primaryDark80,
+          '100': Colors.primaryDark100,
+        }}
+      />
+      <ColorItem
+        title="Secondary"
+        subtitle="#0057FF"
+        colors={{
+          '20': Colors.secondary20,
+          '40': Colors.secondary40,
+          '60': Colors.secondary60,
+          '80': Colors.secondary80,
+          '100': Colors.secondary100,
+        }}
+      />
+      <ColorItem
+        title="Accent | yellow"
+        subtitle="#FFBA08"
+        colors={{
+          '20': Colors.accentYellow20,
+          '40': Colors.accentYellow40,
+          '60': Colors.accentYellow60,
+          '80': Colors.accentYellow80,
+          '100': Colors.accentYellow100,
+        }}
+      />
+      <ColorItem
+        title="Accent | red"
+        subtitle="#FF2B2B"
+        colors={{
+          '20': Colors.accentRed20,
+          '40': Colors.accentRed40,
+          '60': Colors.accentRed60,
+          '80': Colors.accentRed80,
+          '100': Colors.accentRed100,
+        }}
+      />
+      <ColorItem
+        title="Accent | green"
+        subtitle="#00CB82"
+        colors={{
+          '20': Colors.accentGreen20,
+          '40': Colors.accentGreen40,
+          '60': Colors.accentGreen60,
+          '80': Colors.accentGreen80,
+          '100': Colors.accentGreen100,
+        }}
+      />
+    </ColorPalette>
+  ));
---
+++ src/theme/colors.ts
@@ -0,0 +1,37 @@
+export const Colors = {
+  primary20: 'rgba(0, 2, 89, 0.2)',
+  primary40: 'rgba(0, 2, 89, 0.4)',
+  primary60: 'rgba(0, 2, 89, 0.6)',
+  primary80: 'rgba(0, 2, 89, 0.8)',
+  primary100: 'rgba(0, 2, 89, 1)',
+
+  primaryDark20: 'rgba(23, 24, 59, 0.2)',
+  primaryDark40: 'rgba(23, 24, 59, 0.4)',
+  primaryDark60: 'rgba(23, 24, 59, 0.6)',
+  primaryDark80: 'rgba(23, 24, 59, 0.8)',
+  primaryDark100: 'rgba(23, 24, 59, 1)',
+
+  secondary20: 'rgba(0, 87, 255, 0.2)',
+  secondary40: 'rgba(0, 87, 255, 0.4)',
+  secondary60: 'rgba(0, 87, 255, 0.6)',
+  secondary80: 'rgba(0, 87, 255, 0.8)',
+  secondary100: 'rgba(0, 87, 255, 1)',
+
+  accentYellow20: 'rgba(255, 186, 8, 0.2)',
+  accentYellow40: 'rgba(255, 186, 8, 0.4)',
+  accentYellow60: 'rgba(255, 186, 8, 0.6)',
+  accentYellow80: 'rgba(255, 186, 8, 0.8)',
+  accentYellow100: 'rgba(255, 186, 8, 1)',
+
+  accentRed20: 'rgba(255, 43, 43, 0.2)',
+  accentRed40: 'rgba(255, 43, 43, 0.4)',
+  accentRed60: 'rgba(255, 43, 43, 0.6)',
+  accentRed80: 'rgba(255, 43, 43, 0.8)',
+  accentRed100: 'rgba(255, 43, 43, 1)',
+
+  accentGreen20: 'rgba(0, 203, 130, 0.2)',
+  accentGreen40: 'rgba(0, 203, 130, 0.4)',
+  accentGreen60: 'rgba(0, 203, 130, 0.6)',
+  accentGreen80: 'rgba(0, 203, 130, 0.8)',
+  accentGreen100: 'rgba(0, 203, 130, 1)',
+};
